64855ccb7940de32e6faa29410d4518347d1c4ab
ushi <ushi@honkgong.info>
2014-07-02 23:59:12 +0200

    Do not log passwords

--- config/application.rb
+++ config/application.rb
@@ -24,5 +24,8 @@ module KorbjagdApi
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
     # config.i18n.default_locale = :de
+
+    # Do not log passwords
+    config.filter_parameters += [:password, :password_confirmation]
   end
 end
