8d7a0d094ec856e84f8b83fdcce692d546f78a64
ushi <ushi@honkgong.info>
2014-07-08 19:33:06 +0200

    Send GoodbyeMail in V1::ProfilesController#destroy

--- app/controllers/v1/profiles_controller.rb
+++ app/controllers/v1/profiles_controller.rb
@@ -3,24 +3,25 @@ class V1::ProfilesController < V1::ApplicationController
 
   # GET /v1/profile
   def show
-    render json: @current_user, root: :user, serializer: ProfileSerializer
+    render json: current_user, root: :user, serializer: ProfileSerializer
   end
 
   # PATCH /v1/profile
   def update
-    if @current_user.update_attributes(user_params)
-      render json: @current_user, root: :user, serializer: ProfileSerializer
+    if current_user.update_attributes(user_params)
+      render json: current_user, root: :user, serializer: ProfileSerializer
     else
-      render_error 422, @current_user.errors
+      render_error 422, current_user.errors
     end
   end
 
   # DELETE /v1/profile
   def destroy
-    if @current_user.destroy
+    if current_user.destroy
+      GoodbyeMailJob.new.async.perform(current_user)
       render json: nil, status: 204
     else
-      render_error 422, @current_user.errors
+      render_error 422, current_user.errors
     end
   end
 
@@ -30,11 +31,11 @@ class V1::ProfilesController < V1::ApplicationController
   def user_params
     params
       .require(:user)
-      .permit(*policy(@current_user).permitted_attributes)
+      .permit(*policy(current_user).permitted_attributes)
   end
 
   # Authorizes an action on the current user
   def authorize
-    super(@current_user)
+    super(current_user)
   end
 end
