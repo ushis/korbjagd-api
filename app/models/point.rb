6d8363910b63bf6f7a79c9e6070106bd3f8e3ed6
ushi <ushi@honkgong.info>
2014-07-20 05:46:58 +0200

    Removed unused code from the Point class
    - Removed Point.parse_all
    - Removed Point.from_s

--- app/models/point.rb
+++ app/models/point.rb
@@ -4,27 +4,6 @@ class Point < Struct.new(:latitude, :longitude)
   validates :latitude,  presence: true, numericality: true
   validates :longitude, presence: true, numericality: true
 
-  # Parses multiple point strings.
-  def self.parse_all(strings)
-    Array.wrap(strings).map { |s| from_s(s) rescue nil }.compact
-  end
-
-  # Parses a point string.
-  #
-  #   Point.from_s('123.1234523,13.21355')
-  #   #=> #<struct Point latitude=123.1234523, longitude=13.21355>
-  #
-  # Raises ArgumentError for invalid input.
-  def self.from_s(s)
-    coords = s.split(',')
-
-    if coords.length != 2
-      raise ArgumentError.new("Invalid point string: #{s}")
-    else
-      new(*coords.map { |c| Float(c) })
-    end
-  end
-
   alias :lat  :latitude
   alias :lat= :latitude=
 
